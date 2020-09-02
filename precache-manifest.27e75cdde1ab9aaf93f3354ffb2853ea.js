69373fa8d08f563b43d7991197937881504f4fdd
Joel <yantornojoel@gmail.com>
2020-09-02 14:36:12 -0300

    Updates

---
+++ precache-manifest.27e75cdde1ab9aaf93f3354ffb2853ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22e5981d9ff82e7c834d2a9c63480828",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "2cd0445fc24a1ed7dad8",
+    "url": "/React/static/js/2.043dad82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React/static/js/2.043dad82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8e7e71c14bc2afca80e",
+    "url": "/React/static/js/main.2bc975eb.chunk.js"
+  },
+  {
+    "revision": "009e279be7e098e7a0da",
+    "url": "/React/static/js/runtime-main.8ff86afa.js"
+  }
+]);
